7a9e1f516e25d1ae96222d4205e300c1ece4f7f4
warrior <82755153+yashkumarkalyan@users.noreply.github.com>
2023-10-07 14:17:34 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task_manager.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'your-secret-key'  # Replace with your own secret key
+
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+api = Api(app)
+
+if __name__ == '__main__':
+    from resources.task import TaskResource, TaskListResource
+    from resources.user import UserResource, UserRegisterResource
+    
+    db.create_all()
+    
+    api.add_resource(TaskListResource, '/tasks')
+    api.add_resource(TaskResource, '/tasks/<int:task_id>')
+    api.add_resource(UserResource, '/users/<int:user_id>')
+    api.add_resource(UserRegisterResource, '/register')
+    
+    app.run(debug=True)
